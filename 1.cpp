32289917a805f666155c990f66f71658d9b31984
inomzhon <inomzhon.abdulloev@mail.ru>
2019-03-20 11:51:00 +0700

    closed #3

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdlib.h>
 #include <pthread.h>
-
+#include <stdio.h>
 int i = 0;
 void* thread_func(void *arg){
       pthread_setcancelstate(PTHREAD_CANCEL_DISABLE,NULL);
       for (i=0; i<4; i++){
-                     printf("I'm still running!\n");
+                     printf("I`m still running!\n");
                      sleep(1);
  
       }
@@ -15,14 +15,14 @@ void* thread_func(void *arg){
       printf("YOU WILL NOT STOP ME!!!\n");
 
 }
-int main (int argc, char * argv[]){
+int main(int argc, char * argv[]){
       pthread_thread;
       pthread_create(&thread,NULL,thread_func,NULL);
-      while(i<1)sleep(1);
+      while(i < 1)sleep(1);
       pthread_cancel(thread);
       printf("Requested to canel the thread\n");
       pthread_join(thread,NULL);
       printf("The thread is stopped.\n");
-retum EXIT_SUCCESS;
+return EXIT_SUCCESS;
 
 }
