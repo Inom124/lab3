ff08663889d5e7b1eb924eac600eb54d0840d110
inomzhon <inomzhon.abdulloev@mail.ru>
2019-03-20 11:21:54 +0700

    closed #2

--- 1.cpp
+++ 1.cpp
@@ -1,27 +1,28 @@
 #include <stdlib.h>
 #include <stdlib.h>
-#include <stdlib.h>
+#include <pthread.h>
+
 int i = 0;
-void* thread_func(void*arg){
-   phtread_setcancelstate(PTHREAD_CANEL_DISABLE,NULL);
-   for (i=0; i<4; i++){
-                  printf("I'm still renning!\n");
-                  sleep(1);
+void* thread_func(void *arg){
+      pthread_setcancelstate(PTHREAD_CANCEL_DISABLE,NULL);
+      for (i=0; i<4; i++){
+                     printf("I'm still running!\n");
+                     sleep(1);
  
-  }
-  pthread_setcancelstate(PTHREAD_CANEL_ENABLE,NULL);
-  pthread_testcanel();
-printf("YOU WILL NOT STOP ME!!!\n";
+      }
+      pthread_setcancelstate(PTHREAD_CANCEL_ENABLE,NULL);
+      pthread_testcancel();
+      printf("YOU WILL NOT STOP ME!!!\n");
 
 }
 int main (int argc, char * argv[]){
-  pthread_thread;
-  pthread_create(&thread,NULL,thread_func,NULL);
-  while(i<1)sleep(1);
-  pthread_cacel(thread);
-  printf("Requested to canel the thread\n");
-  pthread_join(thread,NULL);
-  printf("The thread is stopped.\n");
- retum EXIT_SUCCESS;
+      pthread_thread;
+      pthread_create(&thread,NULL,thread_func,NULL);
+      while(i<1)sleep(1);
+      pthread_cancel(thread);
+      printf("Requested to canel the thread\n");
+      pthread_join(thread,NULL);
+      printf("The thread is stopped.\n");
+retum EXIT_SUCCESS;
 
 }
